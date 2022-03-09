255e65aeb3df68e89553760ba9e7f488fdf8a336
DarrelA <darrelaiscoding@gmail.com>
2022-03-09 18:56:56 +0800

    Login setup

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -4,6 +4,9 @@ import {
   REGISTER_USER_BEGIN,
   REGISTER_USER_ERROR,
   REGISTER_USER_SUCCESS,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_ERROR,
+  LOGIN_USER_SUCCESS,
 } from './actions';
 
 const reducer = (state, action) => {
@@ -52,6 +55,34 @@ const reducer = (state, action) => {
     };
   }
 
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.jobLocation,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Login Successfully! Redirecting...',
+    };
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    };
+  }
   throw new Error(`no such action : $ {action.type}`);
 };
 
