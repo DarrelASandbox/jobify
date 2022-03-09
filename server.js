875ce8e6eb28199718609b9d25de0e0e515287b0
DarrelA <darrelaiscoding@gmail.com>
2022-03-09 17:03:46 +0800

    Installed Morgan to log HTTP request, UnauthenticatedError setup and comparePassword() with .select(+password) for login controller.

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
 import connectDB from './db/connect.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
-// import 'express-async-errors';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+
 dotenv.config();
 app.use(express.json());
 
