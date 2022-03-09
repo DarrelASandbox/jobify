022a5de402f6e8375e882c25a0eda97f025b9521
DarrelA <darrelaiscoding@gmail.com>
2022-03-09 21:38:19 +0800

    Setup dashboard page with ShareLayout for AddJob, AllJob, Profile and Stats.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Dashboard, Error, Landing, Register } from './pages/index';
+import {
+  AddJob,
+  AllJob,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/dashboard/index';
+import { Error, Landing, Register } from './pages/index';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard />} />
+          <Route path='/' element={<SharedLayout />}>
+            <Route index element={<Stats />} />
+            <Route path='all-jobs' element={<AllJob />} />
+            <Route path='add-job' element={<AddJob />} />
+            <Route path='profile' element={<Profile />} />
+          </Route>
+
           <Route path='/register' element={<Register />} />
           <Route path='/landing' element={<Landing />} />
           <Route path='*' element={<Error />} />
---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+import AddJob from './AddJob.js';
+import AllJob from './AllJob.js';
+import Profile from './Profile.js';
+import SharedLayout from './SharedLayout.js';
+import Stats from './Stats.js';
+
+export { AddJob, AllJob, Profile, SharedLayout, Stats };
