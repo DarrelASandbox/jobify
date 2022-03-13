86071e97f803d6cf301453edc5670d78612bd61c
DarrelA <darrelaiscoding@gmail.com>
2022-03-13 10:07:56 +0800

    Security packages: helmet, xss-clean, express-mongo-sanitize & express-rate-limiter for register and login routes.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import rateLimiter from 'express-rate-limit';
 import { login, register, updateUser } from '../controller/authController.js';
 import authenticateUser from '../middleware/auth.js';
 
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message:
+    'Too many requests from this IP address, please try again after 15 minutes',
+});
+
 const router = express.Router();
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/register').post(apiLimiter, register);
+router.route('/login').post(apiLimiter, login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
 export default router;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 import morgan from 'morgan';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import xss from 'xss-clean';
 import connectDB from './db/connect.js';
 import authenticateUser from './middleware/auth.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -23,6 +25,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.get('/api/v1', (req, res) => {
   res.send('fallback');
