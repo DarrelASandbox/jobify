e9d355c10b22a7303cfa1e9ac9ad277e08d4fda5
DarrelA <darrelaiscoding@gmail.com>
2022-03-13 09:52:34 +0800

    Build frontend app.

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
-}
+if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 
 dotenv.config();
-app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+app.use(express.json());
 
 app.get('/api/v1', (req, res) => {
   res.send('fallback');
@@ -29,6 +30,10 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
